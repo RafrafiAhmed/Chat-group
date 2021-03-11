f962249a387f2167f1f3cc245ff0260711a626c2
RafrafiAhmed <ahmed.rafrafi25@gmail.com>
2021-03-11 22:27:26 +0100

    test

--- ServerSide/app.js
+++ ServerSide/app.js
@@ -13,7 +13,7 @@ var app = express();
 //database connection setup
 const mongoose = require("mongoose");
 mongoose.Promise = global.Promise;
-mongoose.connect("mongodb://localhost:27017/ChatApplication", { useNewUrlParser: true, useUnifiedTopology: true })
+mongoose.connect("mongodb+srv://ahmed_27:AHMEDahmed12345@mean.6ecvi.mongodb.net/mychatd-app?retryWrites=true&w=majority", { useNewUrlParser: true, useUnifiedTopology: true })
   .then(() => console.log('Connected to MongoDB!!'))
   .catch((err) => console.log(err));
  
